07da84fbaf260a6c2aa0a1c402230a4986fc7914
João Paulo Rodrigues <joaopaulo.dev@hotmail.com>
2024-10-04 15:19:50 -0300

    debug false

--- app_core/settings.py
+++ app_core/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^&q5u7))^b$hzoefo9z347&ml&5vrvmy*_dgz)4iylenv!p$rd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
