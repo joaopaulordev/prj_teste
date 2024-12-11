2661f4b87c9f09f44b2fa398d09daddb6b63e2e8
João Paulo Rodrigues <joaopaulo.dev@hotmail.com>
2024-12-11 17:52:07 -0300

    single-app

--- website/views.py
+++ website/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def homepage(request):
             
-    context = {"empresa": "JPR-Soluções em Produção - VPS!!!!!!!!"}
+    context = {"empresa": "JPR-Soluções em Produção - VPS - alterado!!!!!!!!"}
     return render(request, 'homepage.html', context)
